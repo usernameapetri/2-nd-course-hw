5380b3a3494fc13ba07f1ad45c052d9b247b48ad
usernameapetri <apetrimihai772@gmail.com>
2023-06-22 22:29:45 +0200

    Задание 5

--- script.js
+++ script.js
@@ -1,12 +1,5 @@
-let a = 10;
 let b = 2;
 
-let sum = a + b;
-let difference = a - b;
-let product = a * b;
-let quotient = a / b;
+let result = b ** 5;
 
-alert(`sum is ${sum}`);
-alert(`difference is ${difference}`);
-alert(`product is ${product}`);
-alert(`quotient is ${quotient}`);
+alert(result);
