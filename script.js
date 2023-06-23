85a0b980b44282ef6737f32d78f2c8ca806dee8d
usernameapetri <apetrimihai772@gmail.com>
2023-06-23 21:19:59 +0200

    Исправление дз

--- script.js
+++ script.js
@@ -1,3 +1,67 @@
-let name = prompt("Как мне к вам обращаться");
+/*
+Задание 1 
+let a = 10;
+a = 20;
+alert(a);
 
+Задание 2
+const iphoneRelease = 2007;
+alert(iphoneRelease);
+
+Задание 3
+const founderJs = "Brendan Eich";
+alert(founderJs);
+
+Задание 4
+let a = 10;
+let b = 2;
+let sum = a + b;
+let difference = a - b;
+let product = a * b;
+let quotient = a / b;
+alert(`sum is ${sum}`);
+alert(`difference is ${difference}`);
+alert(`product is ${product}`);
+alert(`quotient is ${quotient}`);
+
+Задание 5
+let b = 2;
+let result = b ** 5;
+alert(result);
+
+Задание 6
+let a = 9;
+let b = 2;
+let result = a % b;
+alert(result);
+
+Задание 7 
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num++;
+num--;
+alert(num);
+
+Задание 8
+let age = Number(prompt(`Солько вам лет ?`));
+alert(age);
+
+Задание 9
+const user = {
+  name: "Octavian",
+  age: 25,
+  isAdmin: true,
+};
+user.cityofResidence = "Chisinau";
+user.age = 15;
+delete user.cityofResidence;
+let info = prompt("Какую информацию хотите узнать о пользователе?");
+alert(user[info]);
+
+Задание 10
+let name = prompt("Как мне к вам обращаться");
 alert(`Привет ${name} !`);
+*/
