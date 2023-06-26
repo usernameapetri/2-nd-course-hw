0cdf922cc3ea4a54b2e8ace8fa743560c886e398
usernameapetri <apetrimihai772@gmail.com>
2023-06-26 21:16:02 +0200

    Задание 5

--- script.js
+++ script.js
@@ -108,3 +108,63 @@ let b = "3";
 // Код выше изменять менять нельзя, чтобы решить задачу исправьте код ниже:
 alert(Number(a) + Number(b));
 */
+
+//Задание 5
+
+let monthNumber = Number(
+  prompt("Введите номер месяца и узнайко к какому сезону он принадлежит")
+);
+
+if (monthNumber < 13 && monthNumber > 0) {
+  switch (monthNumber) {
+    case 1:
+      alert("Зима");
+
+      break;
+    case 2:
+      alert("Зима");
+
+      break;
+    case 3:
+      alert("Весна");
+
+      break;
+    case 4:
+      alert("Весна");
+
+      break;
+    case 5:
+      alert("Весна");
+
+      break;
+    case 6:
+      alert("Лето");
+
+      break;
+    case 7:
+      alert("Лето");
+
+      break;
+    case 8:
+      alert("Лето");
+
+      break;
+    case 9:
+      alert("Осень");
+
+      break;
+    case 10:
+      alert("Осень");
+
+      break;
+    case 11:
+      alert("Осень");
+
+      break;
+    case 12:
+      alert("Зима");
+      break;
+  }
+} else {
+  alert("Вы ввели не правельный номер месяца ");
+}
